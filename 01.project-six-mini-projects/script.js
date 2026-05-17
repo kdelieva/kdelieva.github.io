eb72921eb6d8ba083dd303e61d7a9b1defb5d1b7
Katya D. <k.delieva21@gmail.com>
2026-05-17 12:09:27 +0300

    Update the live demo code

--- 01.project-six-mini-projects/script.js
+++ 01.project-six-mini-projects/script.js
@@ -1,1 +1,69 @@
-'use strict';
+"use strict";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 
+// const bill = document.querySelector("#bill");
+// const result = document.querySelector(".tip-bold"); 
+// const buttons = document.querySelectorAll('.tip-percent');
+
+// 
+// function getSelectedTipPercent() {
+//     const selectedButton = document.querySelector('.tip-percent.selected');
+//     if (selectedButton) {
+//         return Number(selectedButton.value); 
+//     }
+//     return 0;
+// }
+
+// function tipCalc(billValue, percentValue) {
+//     return billValue * (percentValue / 100);
+// }
+
+//
+// function updateUI() {
+//     const billValue = Number(bill.value);
+//     const percentValue = getSelectedTipPercent();
+//     const finalTip = tipCalc(billValue, percentValue);
+    
+// 
+//     result.textContent = `${finalTip.toFixed(2)} eur`;
+// }
+
+//
+// bill.addEventListener("input", () => {
+//     updateUI(); 
+// });
+
+// buttons.forEach(button => {
+//     button.addEventListener('click', function(e) {
+//         e.preventDefault(); // Спира презареждането на формата
+        
+//         const currentSelected = document.querySelector('.selected');
+//         if (currentSelected) {
+//             currentSelected.classList.remove('selected');
+//         }
+
+//         this.classList.add('selected');
+
+// 
+//         updateUI(); 
+//     });
+// });
